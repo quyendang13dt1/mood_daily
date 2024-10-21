de94c39cac792e36ff6db091b37af46b075c2f01
Quyen Dang <qdang@atomiton.com>
2024-10-21 16:28:18 +0700

    - update routes

--- mood_daily/src/app/app.routes.ts
+++ mood_daily/src/app/app.routes.ts
@@ -1,3 +1,50 @@
-import { Routes } from '@angular/router';
+// import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+// export const appRoutes: Routes = [
+//   {
+//     path: 'dashboard',
+//     loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+//   },
+// ];
+
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(
+//       appRoutes,
+//     //   {
+//     //     enableTracing: false,
+//     //     // preloadingStrategy: SelectivePreloadingStrategyService,
+//     //   }
+//     )
+//   ],
+//   exports: [
+//     RouterModule
+//   ]
+// })
+// export class AppRoutingModule { }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
+
+export const routes: Routes = [
+ {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+];
