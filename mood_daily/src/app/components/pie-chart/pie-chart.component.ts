78a54a2c42e96371a9c572d2b575ccb2459e3b0a
quyen.dang <qdang@atomiton.com>
2024-10-26 10:43:59 +0700

    update pipe chart

--- mood_daily/src/app/components/pie-chart/pie-chart.component.ts
+++ mood_daily/src/app/components/pie-chart/pie-chart.component.ts
@@ -1,63 +1,47 @@
 import { CommonModule, DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+} from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import { debug } from 'console';
-import { MOOD_STATUS } from '../mood-form/mood-form.component';
+import { MOOD_STATUS, MOOD_STATUS_KEY } from '../mood-form/mood-form.component';
+import { ChartModule } from 'primeng/chart';
+import { FieldsetModule } from 'primeng/fieldset';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
 
 Chart.register(...registerables);
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ChartModule,
+    FieldsetModule,
+    FormsModule,
+    RatingModule,
+  ],
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss',
 })
 export class PieChartComponent implements OnInit, AfterViewInit {
-  data = {
-    labels: ['Red', 'Blue', 'Yellow'],
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)',
-        ],
-        hoverOffset: 4,
-      },
-    ],
-  };
-  config: any = {
-    type: 'doughnut',
-    data: this.data,
-    options: {
-      rotation: -90,
-      circumference: 180,
-      cutout: '70%', // Cutout percentage for a 180-degree chart
-      plugins: {
-        legend: {
-          display: false, // Hide the legend if not needed
-        },
-      },
-    },
-    responsive: true,
-    maintainAspectRatio: false, // Không duy trì tỷ lệ khung mặc định
-    layout: {
-      padding: {
-        top: 0,
-        bottom: -50, // Thu gọn khoảng trống phía dưới
-      },
-    },
-  };
+  data: any;
+  config: any = {};
   chart: any;
-  moodList: any[] = Object.values(MOOD_STATUS);
+  moodList: any[] = Object.values(MOOD_STATUS)?.reverse();
+
+  statusToday = MOOD_STATUS[MOOD_STATUS_KEY.HAPPPY];
   constructor(@Inject(DOCUMENT) private document: Document) {
     // var ctx = (this.document.getElementById("pie-chart") as any)?.getContext("2d");
-    this.chart = new Chart('pipeChart', this.config);
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadChart();
+  }
 
   ngAfterViewInit(): void {
     //     const canvas = document.getElementById('pipeChart') as HTMLCanvasElement;
@@ -67,5 +51,59 @@ export class PieChartComponent implements OnInit, AfterViewInit {
     // if (canvas) {
     //   this.chart = new Chart(canvas, this.config);
     // }
+  }
+
+  loadChart() {
+    this.data = {
+      labels: this.moodList?.map((x) => x?.name),
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: this.moodList?.map((x) => 20),
+          backgroundColor: this.moodList?.map((x) => x?.color),
+          hoverOffset: 4,
+        },
+      ],
+    };
+    this.config = {
+      type: 'doughnut',
+      data: this.data,
+      options: {
+        rotation: -90,
+        circumference: 180,
+        cutout: '70%', // Cutout percentage for a 180-degree chart
+        plugins: {
+          legend: {
+            display: false, // Hide the legend if not needed
+          },
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false, // Không duy trì tỷ lệ khung mặc định
+      layout: {
+        padding: {
+          top: 0,
+          bottom: -50, // Thu gọn khoảng trống phía dưới
+        },
+      },
+    };
+    this.chart = new Chart('pipeChart', this.config);
+
+    // const rotateBox = this.document.getElementById('rotateBox');
+
+    // if (rotateBox) {
+    //   // rotateBox.style.setProperty('--rotation-start', `${202}deg`);
+    //   // rotateBox.style.setProperty(
+    //   //   '--rotation-end',
+    //   //   `${202 + 36 * (this.statusToday?.value - 1)}deg`
+    //   // );
+    //   // rotateBox.style.animation = `rotate 2s ease-in-out forwards`;
+    //   console.log('rotateBox', rotateBox);
+    // }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.loadChart();
   }
 }
